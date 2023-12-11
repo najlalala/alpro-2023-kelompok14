30f53be9841cb12313e2549a70bc15805d7eb46b
Najla Dhia <109951025+najlalala@users.noreply.github.com>
2023-12-11 19:37:31 +0700

    update

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import pickle
 import pandas as pd
-from flask import Flask, request, jsonify
-from flask import render_template
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
@@ -37,12 +36,15 @@ def load_ohe():
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('Fibonacci.html')
 
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
-@app.route('/form')
-def form():
-    return render_template('pred_form.html')
+@app.route('/input')
+def input():
+    return render_template('input.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -52,12 +54,12 @@ def predict():
     # Extracting data from the JSON request
     gender = request.form.get("gender")
     age = request.form.get("age")
+    weight = float(request.form.get("weight"))
+    height = float(request.form.get("height")) 
     sleep_duration = request.form.get("sleep_duration")
     quality_of_sleep = request.form.get("quality_of_sleep")
-    physical_activity = request.form.get("physical_activity")
-    stress_level = request.form.get("stress_level")
-    weight = float(request.form.get("weight"))  
-    height = float(request.form.get("height"))  
+    activity = float(request.form.get("activity"))
+    stress = request.form.get("stress")
     heart_rate = request.form.get("heart_rate")
     daily_steps = request.form.get("daily_steps")
 
@@ -75,6 +77,9 @@ def predict():
     else:
         bmi_category = "Obese"
 
+    # Ubah ke Menit
+    physical_activity = activity * 60
+
     # Creating a DataFrame from the JSON data
     data = pd.DataFrame({
         "Gender": [gender],
@@ -82,8 +87,8 @@ def predict():
         "Sleep Duration": [sleep_duration],
         "Quality of Sleep": [quality_of_sleep],
         "Physical Activity Level": [physical_activity],
-        "Stress Level": [stress_level],
-        "BMI Category": [bmi_category],  
+        "Stress Level": [stress],
+        "BMI Category": [bmi_category],
         "Heart Rate": [heart_rate],
         "Daily Steps": [daily_steps]
     })
@@ -92,9 +97,25 @@ def predict():
     processed_data.columns = processed_data.columns.astype(str)
     prediction = model.predict(processed_data)
     result = index_to_label(prediction[0])
+    result = str(result)
     
-    return render_template('pred_result.html', prediction=result)
+    return render_template('insomnia.html', result=result)
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/information')
+def information():
+    return render_template('information.html')
+
+@app.route('/recommendapp')
+def recommendapp():
+    return render_template('recommendapp.html')
+
+@app.route('/ourteam')
+def ourteam():
+    return render_template('ourteam.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
